e2de8ea10d3bd726f98203db61c5324e504cb82c
Thomas Wehmöller <connect.mordrag@gmx.de>
2021-10-24 00:57:30 +0200

    🐛 Fix nix flake

--- flake.nix
+++ flake.nix
@@ -5,37 +5,35 @@
     fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, fenix }: {
+  outputs = { self, nixpkgs, utils, naersk, fenix }: let
     overlay = (final: prev: {
       gitmoji = prev.callPackage (import ./default.nix) {
         inherit naersk fenix;
       };
     });
+  in { overlay = overlay; } // utils.lib.eachDefaultSystem (system: let
+    pkgs = nixpkgs.legacyPackages."${system}";
+    naersk-lib = naersk.lib."${system}";
+  in rec {
+    # `nix build`
+    packages.gitmoji = import ./default.nix {
+      inherit system;
+      inherit (nixpkgs) lib;
+      inherit pkgs;
+      inherit naersk fenix;
+    };
       
-    gitmoji = utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages."${system}";
-      naersk-lib = naersk.lib."${system}";
-    in rec {
-      # `nix build`
-      packages.gitmoji = import ./default.nix {
-        inherit system;
-        inherit (nixpkgs) lib;
-        inherit pkgs;
-        inherit naersk fenix;
-      };
-        
-      defaultPackage = packages.gitmoji;
-  
-      # `nix run`
-      apps.gitmoji = utils.lib.mkApp {
-        drv = packages.gitmoji;
-      };
-      defaultApp = apps.gitmoji;
-  
-      # `nix develop`
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [ rustc cargo ];
-      };
-    });
-  };
+    defaultPackage = packages.gitmoji;
+
+    # `nix run`
+    apps.gitmoji = utils.lib.mkApp {
+      drv = packages.gitmoji;
+    };
+    defaultApp = apps.gitmoji;
+
+    # `nix develop`
+    devShell = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ rustc cargo ];
+    };
+  });
 }
