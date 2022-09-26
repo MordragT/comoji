5b820a69577137d7a2fc345d90b6be2fadf7eaa9
Thomas Wehmöller <connect.mordrag@gmx.de>
2022-09-26 15:28:53 +0200

    ✨ Update flake and simplify flake

--- flake.nix
+++ flake.nix
@@ -1,45 +1,62 @@
 {
+  description = "Create beautiful git commit messages";
+
   inputs = {
     utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
-    fenix.url = "github:nix-community/fenix";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, fenix }: let
-    overlay = (final: prev: {
-      comoji = prev.callPackage (import ./default.nix) {
-        inherit naersk fenix;
-      };
-    });
-  in { overlay = overlay; } // utils.lib.eachDefaultSystem (system: let
-    pkgs = nixpkgs.legacyPackages."${system}";
-    naersk-lib = naersk.lib."${system}";
-    toolchain = fenix.packages.${system}.complete;
-  in rec {
-    # `nix build`
-    packages.comoji = import ./default.nix {
-      inherit system;
-      inherit (nixpkgs) lib;
-      inherit pkgs;
-      inherit naersk fenix;
-    };
-    packages.default = packages.comoji;
+  outputs = { self, nixpkgs, utils, fenix }:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ fenix.overlay ];
+          };
+          toolchain = pkgs.fenix.complete;
+        in
+        rec {
+          packages.default = (pkgs.makeRustPlatform {
+            inherit (toolchain) cargo rustc;
+          }).buildRustPackage {
+            pname = "comoji";
+            version = "1.2.0";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
 
-    # `nix run`
-    apps.comoji = utils.lib.mkApp {
-      drv = packages.comoji;
-    };
-    apps.default = apps.comoji;
+            meta = with pkgs.lib; {
+              description = "Create beautiful git commit messages";
+              homepage = "https://github.com/MordragT/comoji";
+              license = licenses.mit;
+              maintainers = with maintainers; [ mordrag ];
+              mainProgram = "comoji";
+            };
+          };
 
-    # `nix develop`
-    devShell = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        (toolchain.withComponents [
-          "cargo" "rustc" "rust-src" "rustfmt" "clippy"    
-        ])
-        openssl
-        pkgconfig
-      ];
-    };
-  });
+          apps.default = utils.lib.mkApp {
+            drv = packages.default;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              (with toolchain; [
+                cargo
+                rustc
+                rust-src
+                clippy
+                rustfmt
+              ])
+              openssl
+              pkg-config
+            ];
+          };
+
+          overlays.default = self: pkgs: {
+            comoji = packages.default;
+          };
+        });
 }
