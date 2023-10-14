4d68be98f615b435b26d849d33d2c3620d6cd27b
Thomas Wehmöller <connect.mordrag@gmx.de>
2023-10-14 12:46:13 +0200

    🎉 Release 1.3.0

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     in rec {
       packages.default = pkgs.rustPlatform.buildRustPackage {
         pname = "comoji";
-        version = "1.2.2";
+        version = "1.3.0";
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
 
